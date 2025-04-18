9db0b22e17109648f3fea0321267e7bfc90eaaa2
cmmbert <cmmbert@gmail.com>
2025-04-18 20:14:32 +0200

    First Bounce

--- Assets/Scripts/GroundChecker.cs
+++ Assets/Scripts/GroundChecker.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class GroundChecker : MonoBehaviour
 {
     public bool IsGrounded;
-
+    [SerializeField] float _rayLength = 0.2f;
     private void Update()
     {
-        IsGrounded = Physics.Raycast(transform.position + new Vector3(0, 0.01f), Vector3.down, 0.10f);
-        Debug.DrawRay(transform.position, Vector3.down * 0.10f, Color.red);
+        IsGrounded = Physics.Raycast(transform.position, Vector3.down, _rayLength);
+        Debug.DrawRay(transform.position, Vector3.down * _rayLength, Color.red);
         DebugManager.SetText(IsGrounded.ToString());
     }
 }
--- Assets/Scripts/PipController.cs
+++ Assets/Scripts/PipController.cs
@@ -10,8 +10,8 @@ public class PipController : MonoBehaviour, IPlayerControllable
     [SerializeField] Transform _rotationPivot;
     [SerializeField] GroundChecker _groundChecker;
 
-    [SerializeField] float _jumpForceTotal = 500;
-    [SerializeField] float _jumpForceIncrement = 100;
+    [SerializeField] float _jumpRotationModifier = 0.35f;
+    [SerializeField] float _jumpVelocity = 6;
     bool _isJumping = false;
 
     [SerializeField] float _extraGravity = 2;
@@ -43,7 +43,11 @@ public class PipController : MonoBehaviour, IPlayerControllable
             var y = alpha + beta;
             var dir = new Vector2(Mathf.Cos(y), Mathf.Sin(y)) + camera;
             var uitkomst = (dir - camera).normalized;
-            _rotationPivot.rotation = Quaternion.RotateTowards(_rotationPivot.rotation, Quaternion.Euler(new Vector3(0, Mathf.Atan2(uitkomst.x, uitkomst.y) * Mathf.Rad2Deg, 0)), _rotationSpeed * Time.deltaTime);
+            var jumpMovementMultiplier = _isJumping ? _jumpRotationModifier : 1;
+            _rotationPivot.rotation = Quaternion.RotateTowards(
+                _rotationPivot.rotation, 
+                Quaternion.Euler(new Vector3(0, Mathf.Atan2(uitkomst.x, uitkomst.y) * Mathf.Rad2Deg, 0)), 
+                _rotationSpeed * Time.deltaTime * jumpMovementMultiplier);
             var force = new Vector3(uitkomst.x, 0, uitkomst.y) * _moveSpeed * Time.deltaTime;
             _rb.AddForce(force);
         }
@@ -82,18 +86,33 @@ public class PipController : MonoBehaviour, IPlayerControllable
     public void JumpStarted()
     {
         if (!_groundChecker.IsGrounded)
-            return;
-        _rb.AddForce(0, _jumpForceTotal, 0);
+            return; 
+        var newVelocity = _rb.linearVelocity;
+        newVelocity.y = _jumpVelocity;
+        _rb.linearVelocity = newVelocity;
         _isJumping = true;
     }
 
+    bool _jumpHasLeftTheGround = false;
     public void JumpHeld()
     {
+        if (!_groundChecker.IsGrounded)
+            _jumpHasLeftTheGround = true;
+        if (!_jumpHasLeftTheGround)
+            return;
 
+        if (_groundChecker.IsGrounded)
+        {
+            var newVelocity = _rb.linearVelocity;
+            newVelocity.y = Mathf.Max(Mathf.Abs(newVelocity.y), _jumpVelocity);
+            _rb.linearVelocity = newVelocity;
+            _jumpHasLeftTheGround = false;
+        }
     }
 
     public void JumpReleased()
     {
         _isJumping = false;
+        _jumpHasLeftTheGround = false;
     }
 }
