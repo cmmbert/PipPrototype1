f397f1d4a71b974d87f73399e606b0603c7a5805
cmmbert <cmmbert@gmail.com>
2025-04-26 13:25:44 +0200

    Char now turns before moving

--- Assets/Scripts/PipController.cs
+++ Assets/Scripts/PipController.cs
@@ -9,6 +9,8 @@ public class PipController : MonoBehaviour, IPlayerControllable
     [SerializeField] Transform _cameraTransform;
     [SerializeField] Transform _rotationPivot;
     [SerializeField] GroundChecker _groundChecker;
+    [SerializeField] float _facingForceCorrection = 3;
+    [SerializeField] float _brakeValue = 0.9f;
 
     [SerializeField] float _jumpRotationModifier = 0.35f;
     [SerializeField] float _jumpVelocity = 6;
@@ -37,33 +39,46 @@ public class PipController : MonoBehaviour, IPlayerControllable
     {
         if (axis.magnitude > 0)
         {
+            //Definitions
             var pip = new Vector2(transform.position.x, transform.position.z);
             var camera = new Vector2(_cameraTransform.position.x, _cameraTransform.position.z);
             var gevraagdeDir = axis;
             var alpha = Mathf.Atan2(pip.y - camera.y, pip.x - camera.x);
             var beta = Mathf.Atan2(gevraagdeDir.y, gevraagdeDir.x) - (90 * Mathf.Deg2Rad);
+
+            //https://www.geogebra.org/calculator/bwqwwpax
             var y = alpha + beta;
             var dir = new Vector2(Mathf.Cos(y), Mathf.Sin(y)) + camera;
             var uitkomst = (dir - camera).normalized;
+            //Make sure the rotation is slower when bouncing
             var jumpRotationMultiplier = _isBouncing ? _jumpRotationModifier * Mathf.Abs(axis.x): 1;
-            DebugManager.SetText(Mathf.Abs(axis.x).ToString());
+
             _rotationPivot.rotation = Quaternion.RotateTowards(
                 _rotationPivot.rotation, 
                 Quaternion.Euler(new Vector3(0, Mathf.Atan2(uitkomst.x, uitkomst.y) * Mathf.Rad2Deg, 0)), 
                 _rotationSpeed * Time.deltaTime * jumpRotationMultiplier);
-            var force = _rotationPivot.forward * _moveSpeed * Time.deltaTime;
+
+            //Movement
+            //Makes sure movement only starts when the char is looking at the requested distance
+            var forward2d = new Vector2(_rotationPivot.forward.x, _rotationPivot.forward.z);
+            //DebugManager.SetText(uitkomst.ToString() + ", " + forward2d.ToString() + "= " + Vector2.Angle(uitkomst, forward2d).ToString());
+            var angleBetweenRequestedAndForward = Vector2.Angle(uitkomst, forward2d);
+            float angleForceModifier = 1;
+            if (angleBetweenRequestedAndForward > 0) // prevents dividing by 0
+            {
+                angleForceModifier -= angleBetweenRequestedAndForward / 180; //0 when facing away, 1 when facing towards requested angle
+                angleForceModifier = Mathf.Pow(angleForceModifier, _facingForceCorrection);
+            }
+            var force = _rotationPivot.forward * _moveSpeed * Time.deltaTime * angleForceModifier * axis.magnitude;
             _rb.AddForce(force);
         }
-        else
+        //brake
+        if (_groundChecker.IsGrounded)
         {
-            //brake
-            if (_groundChecker.IsGrounded)
-            {
-                var velocity = _rb.linearVelocity;
-                velocity.x *= 0.9f;
-                velocity.z *= 0.9f;
-                _rb.linearVelocity = velocity;
-            }
+            var velocity = _rb.linearVelocity;
+            velocity.x *= _brakeValue;
+            velocity.z *= _brakeValue;
+            _rb.linearVelocity = velocity;
         }
     }
 
