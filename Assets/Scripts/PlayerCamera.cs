aa673b8e5b263229088580c2b65645ca2606528d
cmmbert <cmmbert@gmail.com>
2025-05-01 17:24:41 +0200

    Fixed camera clipping Added vertical camera movement

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] float _overrideRotationSpeedMultiplier = 0.5f;
-    [SerializeField] Transform _cameraRotationPoint;
+    [SerializeField] Transform _horizontalCameraRotationPoint;
+    [SerializeField] Transform _verticalCameraRotationPoint;
     [SerializeField] Transform _anchor;
     [SerializeField] float _lateralRotationSpeed = 45;
     [SerializeField] float _maxDistance = 4;
     [SerializeField] Transform _desiredPosition;
+    [SerializeField] float _maxVerticalAngle = 45;
+    [SerializeField] float _verticalMinMovement = 0.1f;
+    [SerializeField] float _clippingOffset = 0.8f;
 
     Vector2 _requestedRotation;
 
@@ -17,6 +22,9 @@ public class PlayerCamera : MonoBehaviour
         _requestedRotation = axis;
         if (_requestedRotation.x > 1) _requestedRotation.x = 1;
         if (_requestedRotation.x < -1) _requestedRotation.x = -1;
+
+        if (_requestedRotation.y > 1) _requestedRotation.y = 1;
+        if (_requestedRotation.y < -1) _requestedRotation.y = -1;
     }
 
     private void FixedUpdate()
@@ -33,15 +41,35 @@ public class PlayerCamera : MonoBehaviour
     void CameraRotation()
     {
         if (!_isBeingOverriden)
+        {
             if (Mathf.Abs(_requestedRotation.x) > 0)
             {
-                _cameraRotationPoint.Rotate(new Vector3(0, _requestedRotation.x * _lateralRotationSpeed * Time.deltaTime, 0));
+                _horizontalCameraRotationPoint.Rotate(new Vector3(0, _requestedRotation.x * _lateralRotationSpeed * Time.deltaTime, 0));
             }
+            if (Mathf.Abs(_requestedRotation.y) > _verticalMinMovement)
+            {
+                _requestedRotation -= new Vector2(0, _verticalMinMovement) * Mathf.Sign(_requestedRotation.y);
+                _verticalCameraRotationPoint.Rotate(new Vector3(-_requestedRotation.y * _lateralRotationSpeed * Time.deltaTime, 0, 0));
+                var xAngle = _verticalCameraRotationPoint.localRotation.eulerAngles.x;
+                xAngle = Mathf.Clamp(NormalizeAngle(xAngle), -_maxVerticalAngle, _maxVerticalAngle);
+                _verticalCameraRotationPoint.localRotation = Quaternion.Euler(xAngle, 0, 0);
+            }
+        }
+    }
+
+    //return angle in range -180 to 180
+    float NormalizeAngle(float a)
+    {
+        if (a > 180)
+        {
+            return a - 360;
+        }
+        return a;
     }
 
     public void OverrideCameraAngle(float newAngle)
     {
-        _cameraRotationPoint.rotation = Quaternion.RotateTowards(_cameraRotationPoint.rotation, Quaternion.Euler(new Vector3(0, newAngle, 0)), _lateralRotationSpeed * Time.deltaTime * _overrideRotationSpeedMultiplier);
+        _horizontalCameraRotationPoint.rotation = Quaternion.RotateTowards(_horizontalCameraRotationPoint.rotation, Quaternion.Euler(new Vector3(0, newAngle, 0)), _lateralRotationSpeed * Time.deltaTime * _overrideRotationSpeedMultiplier);
         _isBeingOverriden = true;
     }
 
@@ -56,16 +84,16 @@ public class PlayerCamera : MonoBehaviour
         Vector3 direction = _desiredPosition.position - _anchor.position;
         Ray ray = new Ray(_anchor.position, direction);
         Debug.DrawLine(_anchor.position, _anchor.position + direction.normalized * _maxDistance, Color.yellow);
-        DebugManager.SetText(direction.ToString());
         if (Physics.Raycast(ray, out hit, _maxDistance))
         {
-            transform.position = hit.point;
-            DebugManager.SetText("hit");
+            var hitVector = (hit.point - _anchor.position);
+            var hitDistance = (hitVector.magnitude / _maxDistance);
+            DebugManager.SetText(hitDistance.ToString());
+            transform.position = (hitVector * (_clippingOffset * hitDistance)) + _anchor.position;
         }
         else
         {
             transform.position = _desiredPosition.position;
-            DebugManager.SetText("no hit");
         }
     }
 
